7d4952e38d68297124e0ecd669b3f27ca062c1df
Loïc VATIN <vatin.loic@gmail.com>
2026-05-10 15:23:39 +0200

    feat: Resource model

---
+++ lib/data/models/resource_model.dart
@@ -0,0 +1,34 @@
+enum ResourceType { food, wood, stone }
+
+class Resource {
+  final ResourceType type;
+  double amount;
+  double generationRatePerSecond;
+
+  Resource({
+    required this.type,
+    this.amount = 0.0,
+    this.generationRatePerSecond = 0.0,
+  });
+
+  Resource copyWith({
+    ResourceType? type,
+    double? amount,
+    double? generationRatePerSecond,
+  }) {
+    return Resource(
+      type: type ?? this.type,
+      amount: amount ?? this.amount,
+      generationRatePerSecond:
+      generationRatePerSecond ?? this.generationRatePerSecond,
+    );
+  }
+
+  void add(double value) => amount += value;
+
+  void subtract(double value) => amount -= value;
+
+  void upgrade(double value) => generationRatePerSecond += value;
+
+  void downgrade(double value) => generationRatePerSecond -= value;
+}
